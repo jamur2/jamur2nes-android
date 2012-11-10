cf6f60497389b2781ca0f70a88417ac80a01e1cc
John Murphy <jackie.murphy@gmail.com>
2012-11-10 15:17:16 -0500

    - less debuggy account list

---
+++ src/net/jamur2/jamur2nes/AccountEntryAdapter.java
@@ -0,0 +1,43 @@
+package net.jamur2.jamur2nes;
+
+import android.accounts.Account;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class AccountEntryAdapter extends ArrayAdapter<Account> {
+    private ArrayList<Account> accountDataItems;
+    private Activity context;
+
+
+    public AccountEntryAdapter(Activity context, int textViewResourceId,
+            ArrayList<Account> accountDataItems) {
+        super(context, textViewResourceId, accountDataItems);
+        this.context = context;
+        this.accountDataItems = accountDataItems;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater vi = (LayoutInflater)this.context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.list_item, null);
+        }
+
+        Account account = accountDataItems.get(position);
+        TextView emailTextView = (TextView) v.findViewById(
+            R.id.email_text_view);
+        emailTextView.setText(account.name);
+        return v;
+    }
+
+}
--- src/net/jamur2/jamur2nes/LoginActivity.java
+++ src/net/jamur2/jamur2nes/LoginActivity.java
@@ -4,9 +4,11 @@ import android.accounts.*;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.ArrayAdapter;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LoginActivity extends ListActivity
 {
@@ -16,19 +18,24 @@ public class LoginActivity extends ListActivity
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
         accountManager = AccountManager.get(getApplicationContext());
         Account[] accounts = accountManager.getAccountsByType("com.google");
-        this.setListAdapter(new ArrayAdapter(this,
-            R.layout.list_item, accounts));
+        ArrayList<Account> accounts_arraylist = new ArrayList<Account>(
+            Arrays.asList(accounts));
+
+        this.setListAdapter(new AccountEntryAdapter(this,
+            R.layout.list_item, accounts_arraylist));
     }
 
-        @Override
-        protected void onListItemClick(ListView l, View v, int position,
-                long id) {
-            Account account = (Account)getListView().getItemAtPosition(
-                position);
-            Intent intent = new Intent(this, AppInfo.class);
-            intent.putExtra("account", account);
-            startActivity(intent);
-        }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position,
+            long id) {
+        Account account = (Account)getListView().getItemAtPosition(
+            position);
+        Intent intent = new Intent(this, AppInfo.class);
+        intent.putExtra("account", account);
+        startActivity(intent);
+    }
 }
