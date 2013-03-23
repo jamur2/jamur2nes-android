ebf6617e96c3d438e2947a1d23e580db9ee0844e
John Murphy <jackie.murphy@gmail.com>
2013-03-23 11:35:52 -0400

    import cleanup

--- src/net/jamur2/jamur2nes/AccountEntryAdapter.java
+++ src/net/jamur2/jamur2nes/AccountEntryAdapter.java
@@ -3,7 +3,6 @@ package net.jamur2.jamur2nes;
 import android.accounts.Account;
 import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
